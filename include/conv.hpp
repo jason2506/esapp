5510dcfd1213be61068e0197340302239306109b
Chi-En Wu <jason2506.mail@gmail.com>
2014-02-28 21:01:12 +0800

    Inline string converters

--- include/conv.hpp
+++ include/conv.hpp
@@ -18,10 +18,31 @@
 namespace esapp
 {
 
-std::vector<std::wstring> vec_s2ws(std::vector<std::string> const &str);
-std::wstring s2ws(std::string const &str);
-std::vector<std::string> vec_ws2s(std::vector<std::wstring> const &wstr);
-std::string ws2s(std::wstring const &wstr);
+inline std::wstring s2ws(std::string const &str)
+{
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
+    return conv.from_bytes(str);
+}
+
+inline std::vector<std::wstring> vec_s2ws(std::vector<std::string> const &vec)
+{
+    decltype(vec_s2ws(vec)) ws_vec(vec.size());
+    std::transform(vec.begin(), vec.end(), ws_vec.begin(), &s2ws);
+    return ws_vec;
+}
+
+inline std::string ws2s(std::wstring const &wstr)
+{
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
+    return conv.to_bytes(wstr);
+}
+
+inline std::vector<std::string> vec_ws2s(std::vector<std::wstring> const &ws_vec)
+{
+    decltype(vec_ws2s(ws_vec)) vec(ws_vec.size());
+    std::transform(ws_vec.begin(), ws_vec.end(), vec.begin(), &ws2s);
+    return vec;
+}
 
 } // namespace esa
 
