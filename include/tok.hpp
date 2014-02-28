8b22f7d99513f6ca4b1314f37ab10afdd1e73b52
Chi-En Wu <jason2506.mail@gmail.com>
2014-02-28 18:22:31 +0800

    Inline the methods of TokenIterator

--- include/tok.hpp
+++ include/tok.hpp
@@ -33,15 +33,13 @@ class TokenIterator
 {
 public: // Public Type(s)
     typedef std::wstring Sequence;
+    typedef Sequence::const_iterator Iterator;
 
     typedef std::input_iterator_tag iterator_category;
     typedef Sequence value_type;
     typedef ptrdiff_t difference_type;
     typedef value_type const *pointer;
     typedef value_type const &reference;
-
-private: // Private Type(s)
-    typedef Sequence::const_iterator Iterator;
 
 public: // Public Method(s)
     TokenIterator(Sequence const &sequence);
@@ -61,25 +59,10 @@ public: // Public Method(s)
 
 private: // Private Method(s)
     template <typename Predicate>
-    void skip(Predicate pred)
-    {
-        for ( ; begin_ != end_ && pred(*begin_); ++begin_) { /* do nothing */ }
-    }
+    void skip(Predicate pred);
 
     template <typename Predicate>
-    bool scan(Predicate pred)
-    {
-        auto begin = begin_;
-        skip(pred);
-
-        bool scanned = begin != begin_;
-        if (scanned)
-        {
-            token_.assign(begin, begin_);
-        }
-
-        return scanned;
-    }
+    bool scan(Predicate pred);
 
 private: // Private Property(ies)
     Iterator begin_;
@@ -87,6 +70,94 @@ private: // Private Property(ies)
     Sequence token_;
     bool at_end_;
 }; // class TokenIterator
+
+/************************************************
+ * Implementation: class TokenIterator
+ ************************************************/
+
+inline TokenIterator::TokenIterator(Sequence const &sequence)
+    : TokenIterator(sequence.begin(), sequence.end())
+{
+    // do nothing
+}
+
+inline TokenIterator::TokenIterator(Iterator const &begin, Iterator const &end)
+    : begin_(begin), end_(end), at_end_(begin_ == end_)
+{
+    if (!at_end_) { ++(*this); }
+}
+
+inline TokenIterator TokenIterator::end(void) const
+{
+    return TokenIterator(end_, end_);
+}
+
+inline bool TokenIterator::at_end(void) const
+{
+    return at_end_;
+}
+
+inline TokenIterator &TokenIterator::operator++(void)
+{
+    skip(&isspace);
+    if (begin_ == end_)
+    {
+        at_end_ = true;
+        token_.clear();
+    }
+    else if (!scan(&ischs) && !scan(&isalnum))
+    {
+        token_.assign(1, *begin_);
+        ++begin_;
+    }
+
+    return *this;
+}
+
+inline TokenIterator TokenIterator::operator++(int)
+{
+    decltype((*this)++) it(*this);
+    ++(*this);
+    return it;
+}
+
+inline TokenIterator::reference TokenIterator::operator*(void) const
+{
+    return token_;
+}
+
+inline TokenIterator::pointer TokenIterator::operator->(void) const
+{
+    return &token_;
+}
+
+inline bool TokenIterator::operator==(TokenIterator const &it) const
+{
+    return begin_ == it.begin_ && at_end_ == it.at_end_;
+}
+
+inline bool TokenIterator::operator!=(TokenIterator const &it) const
+{
+    return !(*this == it);
+}
+
+template <typename Predicate>
+inline void TokenIterator::skip(Predicate pred)
+{
+    for ( ; begin_ != end_ && pred(*begin_); ++begin_) { /* do nothing */ }
+}
+
+template <typename Predicate>
+inline bool TokenIterator::scan(Predicate pred)
+{
+    auto begin = begin_;
+    skip(pred);
+
+    bool scanned = begin != begin_;
+    if (scanned) { token_.assign(begin, begin_); }
+
+    return scanned;
+}
 
 } // namespace esa
 
