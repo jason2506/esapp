5516e11fb5c092a06c828450c7b59efc119e14f2
Chi-En Wu <jason2506.mail@gmail.com>
2015-05-10 19:22:36 +0800

    Replace distutils with setuptools

--- wrapper/python/setup.py
+++ wrapper/python/setup.py
@@ -1,15 +1,18 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 from Cython.Distutils import build_ext
+
+esapp = Extension(
+    'esapp',
+    sources=['cython/esapp.pyx'],
+    extra_compile_args=['-std=c++11'],
+    install_requires=['cython'],
+    include_dirs=['../../include'],
+    library_dirs=['../../lib'],
+    libraries=['esapp'],
+    language='C++'
+)
 
 setup(
     cmdclass={'build_ext': build_ext},
-    ext_modules=[
-        Extension('esapp',
-                  sources=['cython/esapp.pyx'],
-                  extra_compile_args=['-std=c++11'],
-                  include_dirs=['../../include'],
-                  library_dirs=['../../lib'],
-                  libraries=['esapp'],
-                  language='C++')
-    ]
+    ext_modules=[esapp]
 )
