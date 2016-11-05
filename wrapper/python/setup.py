6ba797ae0603e42f8d734863e3d247087cfdea18
Chi-En Wu <jason2506.mail@gmail.com>
2016-11-05 12:47:57 +0800

    Refine `setup.py` for python wrapper

--- wrapper/python/setup.py
+++ wrapper/python/setup.py
@@ -5,14 +5,12 @@ esapp = Extension(
     'esapp',
     sources=['cython/esapp.pyx'],
     extra_compile_args=['-std=c++11'],
-    install_requires=['cython'],
-    include_dirs=['../../include'],
-    library_dirs=['../../lib'],
     libraries=['esapp'],
     language='C++'
 )
 
 setup(
+    setup_requires=['cython>=0.x'],
     cmdclass={'build_ext': build_ext},
     ext_modules=[esapp]
 )
